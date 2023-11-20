8e45c05384897c7dfde92fa8431db198118408a1
Mohannad <90407906+Mohannad-alafeef@users.noreply.github.com>
2023-11-20 19:16:59 +0300

    fix the code

--- src/app/Services/student.service.ts
+++ src/app/Services/student.service.ts
@@ -6,7 +6,36 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class StudentService {
   constructor(private http: HttpClient) {}
-  Foo = (id: any) => {
-    console.log('from servcie ' + id);
+  getStudent = async (id: any) => {
+    return await new Promise((res, rej) => {
+      this.http
+        .get('http://192.168.233.98:5001/api/UserAccount/user/' + 4)
+        .subscribe({
+          next: (resp: any) => {
+            console.log(resp);
+
+            res(resp);
+          },
+          error: (err) => {
+            rej(err);
+          },
+        });
+    });
+  };
+  getStudentCertifications = async (id: any) => {
+    return await new Promise<any[]>((res, rej) => {
+      this.http
+        .get('http://192.168.233.98:5001/api/UserAccount/UserCourses/' + 4)
+        .subscribe({
+          next: (resp: any) => {
+            console.log(resp);
+
+            res(resp);
+          },
+          error: (err) => {
+            rej(err);
+          },
+        });
+    });
   };
 }
